335be88eada21564fda1cfd7b28e8e5b72d4cafc
tranminhhoang2121050677 <125141355+tranminhhoang2121050677@users.noreply.github.com>
2023-03-05 15:40:19 +0700

    Add files via upload

---
+++ bai thuc hanh 2102/baithuchanh2102.java
@@ -0,0 +1,23 @@
+package baithuchanh;
+
+import java.util.Scanner;
+
+public class BaiThucHanh2102 {
+
+	public static void main(String[] args) {
+		int tong=0;
+		
+		Scanner sc = new Scanner(System.in);
+		System.out.print("nhap so ngong nhien: ");
+		int a = sc.nextInt();
+		
+		while (a>0) {
+		tong += a%10;
+		a /=10;	
+		}
+		
+		System.out.println("tong = " +tong);
+		sc.close();
+	}
+
+}
